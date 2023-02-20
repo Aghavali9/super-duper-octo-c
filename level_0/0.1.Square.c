cea1bc9f7dbe72fbf59621aac315d8eb1f1056ab
M. Aghavali <Aghavali9@gmail.com>
2023-02-20 22:10:49 +0330

    Create 0.1.Square.c

---
+++ level_0/0.1.Square.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n, i, j;
+
+    n = 0;
+    printf("Enter an integer greater than 2: ");
+    scanf("%d",&n);
+
+    if (n < 2)
+    {
+        printf("Entered number is less than 2!");
+        return -1;
+    }
+    printf("\n");
+    for (i=0; i<n; i++)
+    {
+        printf("*");
+    }
+    printf("\n");
+
+    for (i=2; i<n; i++)
+    {
+        printf("*");
+        for (j=2; j<n; j++)
+        {
+            printf(" ");
+        }
+        printf("*\n");
+    }
+    for (i=0; i<n; i++)
+    {
+        printf("*");
+    }
+    printf("\n");
+
+    return 0;
+}
